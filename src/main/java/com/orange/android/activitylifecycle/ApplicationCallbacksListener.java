31fbc90a20f7ad72f7f42a38369762d4bd5584ab
Christophe Maldivi <christophe.maldivi@orange.com>
2015-10-08 12:30:11 +0200

    docs(headers): update with apache v2 headers, copyright Orange

--- src/main/java/com/orange/android/activitylifecycle/ApplicationCallbacksListener.java
+++ src/main/java/com/orange/android/activitylifecycle/ApplicationCallbacksListener.java
@@ -1,12 +1,18 @@
 package com.orange.android.activitylifecycle;
 /*
- * Copyright (C) 2015 Orange
- * Authors: Christophe Maldivi
+ * Copyright (C) Orange
  *
- * This software is the confidential and proprietary information of Orange.
- * You shall not disclose such confidential information and shall use it only
- * in accordance with the terms of the license agreement you entered into
- * with Orange.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import android.app.Activity;
